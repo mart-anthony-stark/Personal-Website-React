4f8a41dd2fb7ff80c69c49cceaf265cf9ee58178
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-17 21:55:07 +0800

    Added banner component

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,27 @@
+import "./banner.css";
+
+const Banner = () => {
+  const handleCursor = (state) => {
+    const cursor = document.querySelector("#cursor");
+    if (state === "enter") {
+      cursor.classList.add("highlight");
+    } else {
+      cursor.classList.remove("highlight");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1
+        onMouseEnter={() => handleCursor("enter")}
+        onMouseLeave={() => handleCursor("leave")}
+        className="name parallax-layer"
+        data-speed="7"
+      >
+        Mart Anthony
+      </h1>
+    </div>
+  );
+};
+
+export default Banner;
