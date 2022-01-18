a57f7bfb2c174206ff53caadf8177f3cd4aca2e5
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-18 20:26:23 +0800

    Added navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,10 @@
-import Navbar from "../components/Navbar";
+import Navbar from "../components/Navbar/Navbar";
 import Banner from "../components/Banner/Banner";
 
 const Home = () => {
   return (
     <div>
-      {/* <Navbar /> */}
+      <Navbar />
       <Banner />
     </div>
   );
