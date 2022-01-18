085350464483d1853815e318313314ebb10ed5f5
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-18 20:26:15 +0800

    Added navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="left">
+          
+      </div>
+      <h3 className="logo">M</h3>
+      <div className="right"></div>
+    </nav>
+  );
+};
+
+export default Navbar;
