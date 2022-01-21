29a3cd7f0257504d2ded804e791f9a36501f9abb
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-21 10:00:50 +0800

    Fixed styling

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,31 +2,12 @@ import "./banner.css";
 import { Link } from "react-router-dom";
 
 const Banner = () => {
-  const handleCursor = (state) => {
-    const cursor = document.querySelector("#cursor");
-    if (state === "enter") {
-      cursor.classList.add("highlight");
-    } else {
-      cursor.classList.remove("highlight");
-    }
-  };
-
   return (
     <div className="home">
-      <h1
-        onMouseEnter={() => handleCursor("enter")}
-        onMouseLeave={() => handleCursor("leave")}
-        className="name parallax-layer"
-        data-speed="7"
-      >
+      <h1 className="name parallax-layer" data-speed="7">
         Mart Anthony
       </h1>
-      <h1
-        onMouseEnter={() => handleCursor("enter")}
-        onMouseLeave={() => handleCursor("leave")}
-        className="name parallax-layer"
-        data-speed="4"
-      >
+      <h1 className="name parallax-layer" data-speed="4">
         Salazar
       </h1>
       <div className="links">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import Navbar from "../components/Navbar/Navbar";
 import Banner from "../components/Banner/Banner";
+import stars from "../images/stars.png";
+import moon from "../images/moon.png";
 
 const Home = () => {
   return (
     <div>
+      <img id="stars" className="parallax-layer" data-speed="8" src={stars} />
+      <img id="moon" data-speed="4" src={moon} />
       <Navbar />
       <Banner />
-      
     </div>
   );
 };
