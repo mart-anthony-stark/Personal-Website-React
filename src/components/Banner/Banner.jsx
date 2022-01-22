fa7ebf3aba8d21135d0a3b0acdd3c912df19523b
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-22 10:34:25 +0800

    Added banner animation

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,16 +1,41 @@
 import "./banner.css";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 const Banner = () => {
   return (
     <div className="home">
-      <h1 className="name parallax-layer" data-speed="7">
+      <motion.h1
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -100 }}
+        transition={{ duration: 1, delay: 2, easings: "linear" }}
+        className="name parallax-layer"
+        data-speed="7"
+      >
         Mart Anthony
-      </h1>
-      <h1 className="name parallax-layer" data-speed="4">
+      </motion.h1>
+      <motion.h1
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        exit={{ y: -100 }}
+        transition={{ duration: 1, delay: 2.8, easings: "linear" }}
+        className="name parallax-layer"
+        data-speed="7"
+        className="name parallax-layer"
+        data-speed="4"
+      >
         Salazar
-      </h1>
-      <div className="links">
+      </motion.h1>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5, delay: 3, easings: "linear" }}
+        className="name parallax-layer"
+        data-speed="7"
+        className="links"
+      >
         <Link to="/works">
           <button>Works</button>
         </Link>
@@ -20,7 +45,7 @@ const Banner = () => {
         <Link to="/contact">
           <button>Contact</button>
         </Link>
-      </div>
+      </motion.div>
     </div>
   );
 };
