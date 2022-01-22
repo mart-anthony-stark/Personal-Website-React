f54aa5fa6402daab34ef7c2c8085ba72e0488b80
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-22 10:34:31 +0800

    Added animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,26 @@ import Navbar from "../components/Navbar/Navbar";
 import Banner from "../components/Banner/Banner";
 import stars from "../images/stars.png";
 import moon from "../images/moon.png";
+import { motion, AnimatePresence } from "framer-motion";
 
 const Home = () => {
   return (
-    <div>
-      <img id="stars" className="parallax-layer" data-speed="8" src={stars} />
-      <img id="moon" data-speed="4" src={moon} />
-      <Navbar />
-      <Banner />
-    </div>
+    <AnimatePresence>
+      <div>
+        <img id="stars" className="parallax-layer" data-speed="8" src={stars} />
+        <motion.img
+          initial={{ y: -100, x: 300, opacity: 0 }}
+          animate={{ y: 0, x: 0, opacity: 1 }}
+          exit={{ y: -100 }}
+          transition={{ duration: 2, easings: "linear" }}
+          id="moon"
+          data-speed="4"
+          src={moon}
+        />
+        <Navbar />
+        <Banner />
+      </div>
+    </AnimatePresence>
   );
 };
 
