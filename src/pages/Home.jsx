56e81ad592358179217f1b01929385662c5e5dca
mart-anthony-stark <martsalazar0@gmail.com>
2022-01-22 12:47:51 +0800

    Added exit animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,25 +3,35 @@ import Banner from "../components/Banner/Banner";
 import stars from "../images/stars.png";
 import moon from "../images/moon.png";
 import { motion, AnimatePresence } from "framer-motion";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+  const [show, setShow] = useState(true);
+
   return (
-    <AnimatePresence>
-      <div>
-        <img id="stars" className="parallax-layer" data-speed="8" src={stars} />
-        <motion.img
-          initial={{ y: -100, x: 300, opacity: 0 }}
-          animate={{ y: 0, x: 0, opacity: 1 }}
-          exit={{ y: -100 }}
-          transition={{ duration: 2, easings: "linear" }}
-          id="moon"
-          data-speed="4"
-          src={moon}
-        />
-        <Navbar />
-        <Banner />
-      </div>
-    </AnimatePresence>
+    <motion.div
+      animate={{ y: 0, x: 0, opacity: 1 }}
+      exit={{ y: -100, opacity: 0 }}
+      transition={{ duration: 2, easings: "linear" }}
+    >
+      <img id="stars" className="parallax-layer" data-speed="8" src={stars} />
+      <AnimatePresence exitBeforeEnter>
+        {show && (
+          <motion.img
+            initial={{ y: -100, x: 300, opacity: 0 }}
+            animate={{ y: 0, x: 0, opacity: 1 }}
+            exit={{ y: -100, opacity: 0 }}
+            transition={{ duration: 2, easings: "linear" }}
+            id="moon"
+            data-speed="4"
+            src={moon}
+          />
+        )}
+      </AnimatePresence>
+
+      <Navbar />
+      <Banner show={show} setShow={setShow} />
+    </motion.div>
   );
 };
 
